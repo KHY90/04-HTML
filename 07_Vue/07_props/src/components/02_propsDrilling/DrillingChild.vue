6324dac95ca0ed931f0ea559cadbff33587fefb6
KHY90 <power4206@gmail.com>
2024-06-03 18:44:42 +0900

    0603

---
+++ 07_Vue/07_props/src/components/02_propsDrilling/DrillingChild.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { inject } from 'vue';
+import Grandson from "./DrillingGrandson.vue";
+
+const propsCount = inject("count");
+
+</script>
+
+<template>
+    <h2>child : {{ propsCount }}</h2>
+    <Grandson/>
+</template>
---
+++ 07_Vue/07_props/src/components/02_propsDrilling/DrillingGrandson.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { inject } from 'vue';
+
+const propsCount = inject("count");
+</script>
+
+<template>
+    <h3>grandson : {{ propsCount }}</h3>
+    <button @click="propsCount++">{{ propsCount }}</button>
+</template>
